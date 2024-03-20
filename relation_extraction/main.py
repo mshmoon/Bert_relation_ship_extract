f1c93fe0381780ed1690f26b860a6c50f065b901
马书浩(10032971) <mash@glodon.com>
2024-03-20 18:17:57 +0800

    ignore

---
+++ relation_extraction/main.py
@@ -0,0 +1,50 @@
+import os
+import time
+import torch
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+from data_utils import SentenceREDataset, get_idx2tag
+from model import SentenceRE
+
+
+def train():
+
+    epochs = 20
+    max_len = 256
+
+    train_batch_size = 32
+    device = torch.device("cuda:0")
+
+    learning_rate = 3e-5
+
+    # train_dataset
+    train_dataset = SentenceREDataset("datasets/train_known.jsonl", "datasets/relation.txt",256)
+    train_loader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+
+    # model
+    idx2tag = get_idx2tag("datasets/relation.txt")
+    tagset_size = len(idx2tag)
+    model = SentenceRE(tagset_size).cuda()
+    model.train()
+
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+
+    for epoch in range(0, epochs):
+        for i_batch, sample_batched in enumerate(tqdm(train_loader, desc='Training')):
+            token_ids = sample_batched['token_ids'].to(device)
+            token_type_ids = sample_batched['token_type_ids'].to(device)
+            attention_mask = sample_batched['attention_mask'].to(device)
+            e1_mask = sample_batched['e1_mask'].to(device)
+            e2_mask = sample_batched['e2_mask'].to(device)
+            tag_ids = sample_batched['tag_id'].to(device)
+            logits = model(token_ids, token_type_ids, attention_mask, e1_mask, e2_mask)
+            loss = criterion(logits, tag_ids)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print(epoch,loss)
+        
+train()
---
+++ relation_extraction/model.py
@@ -0,0 +1,53 @@
+import os
+import logging
+import torch
+import torch.nn as nn
+
+from transformers import BertTokenizer,BertConfig, BertModel
+
+
+class SentenceRE(nn.Module):
+
+    def __init__(self, tagset_size):
+        super(SentenceRE, self).__init__()
+        self.tagset_size = tagset_size
+        self.config = BertConfig()
+        self.bert_model = BertModel.from_pretrained("bert-base-chinese")
+
+        self.dense = nn.Linear(self.config.hidden_size, self.config.hidden_size)
+        self.drop = nn.Dropout(0.05)
+        self.activation = nn.Tanh()
+        self.norm = nn.LayerNorm(self.config.hidden_size * 3)
+        self.hidden2tag = nn.Linear(self.config.hidden_size * 3, self.tagset_size)
+
+    def forward(self, token_ids, token_type_ids, attention_mask, e1_mask, e2_mask):
+        sequence_output, pooled_output = self.bert_model(input_ids=token_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, return_dict=False)
+
+        # 每个实体的所有token向量的平均值
+        e1_h = self.entity_average(sequence_output, e1_mask)
+        e2_h = self.entity_average(sequence_output, e2_mask)
+        e1_h = self.activation(self.dense(e1_h))
+        e2_h = self.activation(self.dense(e2_h))
+
+        # [cls] + 实体1 + 实体2
+        concat_h = torch.cat([pooled_output, e1_h, e2_h], dim=-1)
+        concat_h = self.norm(concat_h)
+        logits = self.hidden2tag(self.drop(concat_h))
+
+        return logits
+
+    @staticmethod
+    def entity_average(hidden_output, e_mask):
+        """
+        Average the entity hidden state vectors (H_i ~ H_j)
+        :param hidden_output: [batch_size, j-i+1, dim]
+        :param e_mask: [batch_size, max_seq_len]
+                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
+        :return: [batch_size, dim]
+        """
+        e_mask_unsqueeze = e_mask.unsqueeze(1)  # [b, 1, j-i+1]
+        length_tensor = (e_mask != 0).sum(dim=1).unsqueeze(1)  # [batch_size, 1]
+
+        sum_vector = torch.bmm(e_mask_unsqueeze.float(), hidden_output).squeeze(1)  # [b, 1, j-i+1] * [b, j-i+1, dim] = [b, 1, dim] -> [b, dim]
+        avg_vector = sum_vector.float() / length_tensor.float()  # broadcasting
+        return avg_vector
